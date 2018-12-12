1f0c914d8366d2fe6545da6a90ff63621a875041
theTrueMikeBrown <michael.brown@coxautoinc.com>
2018-12-12 15:07:32 -0700

    Added link to buttonshy

--- the-perfect-moment/src/App.js
+++ the-perfect-moment/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import Game from './Components/Game';
+import formatImage from "./Domain/formatImage";
 
 class App extends Component {
   render() {
@@ -10,7 +11,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="right">
-            <a href="https://www.dropbox.com/sh/8r91j8724qd6t7v/AACNdDp_w7FixSjqeLAZLJcqa?dl=0&preview=TPM_+Echoes+(SOLO).pdf">rules</a>
+            <a href="https://www.dropbox.com/sh/8r91j8724qd6t7v/AACNdDp_w7FixSjqeLAZLJcqa?dl=0&preview=TPM_+Echoes+(SOLO).pdf">
+              <img className="actionButton" src={formatImage("rules")} alt="Rules" title="Rules" />
+            </a>
+            <a href="https://buttonshygames.com/">
+              <img className="actionButton" src={formatImage("buttonshy")} alt="ButtonShy games" title="ButtonShy games" />
+            </a>
           </div>
         </header>
         <Game />
